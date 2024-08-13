968032383152209a3da5202211b9277f894cbfff
Nauman <inaumanmajeed@gmail.com>
2024-08-13 14:06:52 +0500

    chore: Update form styling for GuessTheNumber game

--- GuessTherNumber/index.js
+++ GuessTherNumber/index.js
@@ -2,23 +2,25 @@ const randomNumber = Math.floor(Math.random() * 100) + 1;
 
 //getting the values
 const submitButton = document.querySelector("#find__number");
+const restartGameBtn = document.querySelector("#restart__number");
 const userInput = document.querySelector("#number");
 const guessesArray = document.querySelector("#previous__guesses");
 const result = document.querySelector("#result");
 const attemptsLeft = document.querySelector("#attempts");
-const p = document.createElement("p");
+// const p = document.createElement("p");
 
 let prevGuesses = [];
-let attempts = 1;
-console.log("🚀 ~ attempts:", attempts)
+// console.log("🚀 ~ prevGuesses:", prevGuesses)
+let attempts = 10;
+// console.log("🚀 ~ attempts:", attempts);
 let playGame = true;
 
 if (playGame) {
   submitButton.addEventListener("click", function (e) {
     e.preventDefault();
-    const userValue = userInput.value.trim(); // Trim any whitespace
-    const userNumber = parseInt(userValue); // Parse the input as an integer
-    console.log("🚀 ~ userNumber:", userNumber);
+    const userValue = userInput.value.trim();
+    const userNumber = parseInt(userValue);
+    // console.log("🚀 ~ userNumber:", userNumber);
     checkNumber(userNumber);
   });
 }
@@ -27,37 +29,85 @@ if (playGame) {
 
 function checkNumber(number) {
   if (isNaN(number)) {
-    console.log("Please enter a valid number");
+    displayMessage(`Please enter a valid number`);
   } else if (number < 1 || number > 100) {
-    console.log("Please enter a number between 1 and 100");
+    displayMessage(
+      `Please enter a number between 1 and 100, your number (${number}) does not fall in this range `
+    );
   } else if (prevGuesses.includes(number)) {
-    console.log("You have already guessed this number");
+    displayMessage(`You have already guessed this number`);
   } else {
-    prevGuesses.push(number);
-    attempts++;
-    checkGuess(number);
+    if (attempts > 0) {
+      prevGuesses.push(number);
+      attempts--;
+      checkGuess(number);
+    } else {
+      displayMessage(`You have reached the maximum number of attempts, the number was ${randomNumber}, better luck next time`);
+      endGame();
+    }
   }
 }
 
 //function to check the attempts left
 
-function checkGuess() {}
+function checkGuess(number) {
+  if (number === randomNumber) {
+    displayMessage("Congratulations! You have guessed the correct number");
+    endGame();
+    displayResult();
+  } else if (number < randomNumber) {
+    displayMessage("Too Low! Try Again");
+    displayPreviousGuesses();
+  } else if (number > randomNumber) {
+    displayMessage("Too High! Try Again");
+    displayPreviousGuesses();
+  }
+}
+
 
 //function to display the result
 
-function displayResult() {}
+function displayResult() {
+  userInput.value = "";
+  guessesArray.innerHTML = `Your previous guesses are:  [${prevGuesses}] `;
+  attemptsLeft.innerHTML = `You have done it in ${10 - attempts} attempts`;
+}
 
 //function to display the message
 
-function displayMessage(message) {}
+function displayMessage(message) {
+  result.innerHTML = message;
+}
 
 //function to display the previous guesses
 
-function displayPreviousGuesses() {}
+function displayPreviousGuesses() {
+  guessesArray.innerHTML = `Your previous guesses are: ${prevGuesses}`;
+  attemptsLeft.innerHTML = `You have ${attempts} attempts left`;
+  userInput.value = "";
+}
 
 //function to restart the game
 
-function restartGame() {}
+function restartGame() {
+  restartGameBtn.addEventListener("click", function (e) {
+    attempts = 1;
+    prevGuesses = [];
+    result.innerHTML = "Let's get started to explore the treasure of curiosity";
+    guessesArray.innerHTML = "";
+    attemptsLeft.innerHTML = "You have 10 attempts to accomplish the target";
+    userInput.value = "";
+    randomNumber = Math.floor(Math.random() * 100) + 1;
+    playGame = true;
+    userInput.removeAttribute("disabled");
+    submitButton.removeAttribute("disabled");
+  });
+}
 //function to end the game
 
-function endGame() {}
+function endGame() {
+  userInput.value = "";
+  userInput.setAttribute("disabled", "");
+  submitButton.setAttribute("disabled", "");
+  playGame = false;
+}
